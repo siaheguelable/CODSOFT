b5a9cf20ef6d0ad13bf80dcb1c9f561951281cf1
siaheguelable <siaheguelable>
2025-04-22 15:36:08 -0700

    code added to js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
+const url = "https://siaheguelable.github.io/CODSOFT/data/projets_info.json";
 
+async function getData() {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+    }
+
+}
+
+getData();
