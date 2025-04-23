e1921c4ae625f9f9c86ff79b67e8ed666e83d084
siaheguelable <siaheguelable>
2025-04-23 04:02:06 -0700

    cv added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,35 +3,50 @@ const projets = document.querySelector('#projects');
 const url = "https://siaheguelable.github.io/CODSOFT/data/projets_info.json";
 
 async function getData() {
-    const response = await fetch(url);
-    if (response.ok) {
-        const data = await response.json();
-        console.log(data.projets);
-    } else {
-        console.error("an error occured while fetching data")
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.projets_info);
+            displayInfo(data.projets_info);
+        } else {
+            console.error("an error occured while fetching data")
+        }
+
+    } catch (error) {
+        console.error("Error fetching links:", error);
+
     }
+
 
 }
 
 
-const displayInfo = (projets) => {
+const displayInfo = (projets_info) => {
 
-    projets.forEach(projet => {
+    projets_info.forEach(info => {
         const card = document.createElement('div');
         card.classList.add('member-card'); // optional for styling
 
         const icon = document.createElement('img');
-        icon.setAttribute('src', projet.image);
-        icon.setAttribute('alt', `Logo of ${projet.name}`);
+        icon.setAttribute('src', info.image);
+        icon.setAttribute('alt', `Logo of ${info.name}`);
         icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '200');
+        icon.setAttribute('height', '300');
 
         const name = document.createElement('p');
-        name.textContent = projet.name;
+        name.textContent = info.name;
 
         const description = document.createElement('p');
-        description.textContent = projet.description;
-        const link = document.createElement('p');
-        link.textContent = projet.link;
+        description.textContent = info.description;
+
+
+
+        const link = document.createElement('a'); // Change this to <a> tag
+        link.href = info.link;
+        link.textContent = "Visit project";
+        link.target = "_blank";
 
         card.appendChild(icon);
         card.appendChild(name);
